126dc7ec433cc17d7adff556761b67f030d92f26
izayl <izayl@163.com>
2020-04-01 22:02:04 +0800

    build(client): support ts

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -4,7 +4,7 @@ const outputDirectory = '../static';
 const path = require('path')
 
 module.exports = {
-  entry: './client/index.jsx',
+  entry: './client/index.tsx',
   output: {
     path: path.join(__dirname, outputDirectory),
     filename: "bundle.js"
